9e57b197220b0bd217a1d9f24d3cdbb1a9496e04
TakesxiSximada <takesxi.sximada@gmail.com>
2015-10-25 10:46:12 +0900

    test

---
+++ client.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import argparse
+import requests
+
+
+def main(argv=sys.argv[1:]):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-e', '--endpoint', default='http://127.0.0.1:8001/api/update')
+    args = parser.parse_args(argv)
+    requests.post(args.endpoint, data={'device_code': 'aaaa', 'hit': 'true'})
+
+if __name__ == '__main__':
+    main()
