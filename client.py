8e20f1efb0459e5854486f62e25deaf90dfff122
TakesxiSximada <takesxi.sximada@gmail.com>
2015-10-25 13:58:13 +0900

    submit

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 import sys
+import json
 import argparse
 import requests
 
@@ -9,7 +10,7 @@ def main(argv=sys.argv[1:]):
     parser = argparse.ArgumentParser()
     parser.add_argument('-e', '--endpoint', default='http://127.0.0.1:8001/api/update')
     args = parser.parse_args(argv)
-    requests.post(args.endpoint, data={'device_code': 'aaaa', 'hit': 'true'})
+    requests.post(args.endpoint, data=json.dumps({'device_code': 'aaaa', 'hit': 'true'}))
 
 if __name__ == '__main__':
     main()
