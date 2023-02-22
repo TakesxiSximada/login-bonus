318e6dd30c36e468cfc36b859aaaefdf4a8ca98a
TakesxiSximada <sximada@gmail.com>
2023-02-22 19:15:20 +0900

    fix: check timestamp for login bonus

--- init.lua
+++ init.lua
@@ -17,10 +17,23 @@ minetest.register_on_joinplayer(function (player)
       local inv = player:get_inventory()
       local login_bonus = ItemStack("default:apple 1")
       local message = string.format("Hi %s, You got a login bonus. (bonus: apple+1)", name)
+      local player_meta = player:get_meta()
+      local last_timestamp = player_meta:get_int("login_bonus:last_timestamp")
+      local current_timestamp = os.time(os.date("!*t"))
+
+      local last_date = os.date("%Y%m%d", last_timestamp)
+      local current_date = os.date("%Y%m%d", current_timestamp)
+
+      -- 当日付与済みであるため付与しない
+      if last_date == current_date then
+	 minetest.chat_send_player(name, "Welcome home")
+	 do return end
+      end
+
       -- 注意
-      -- ログインボーナスと言いつつ、ログインしたら必ず林檎を1つ貰える。
       -- inventoryがいっぱいの場合、ログインボーナスは付与されない。
-      -- ログインボーナスが貰えても、貰えなくてもメッセージは同じように表示される。
+      -- その場合であっても、メッセージは同じように表示される。
       inv:add_item("main", login_bonus)
+      player_meta:set_int("login_bonus:last_timestamp", current_timestamp)
       minetest.chat_send_player(name, message)
 end)
