9f8f8b67032a239ce3228a12cba3e7dbe02a310c
TakesxiSximada <sximada@gmail.com>
2023-02-22 18:51:14 +0900

    feat: player get a apple when login (psude login bonus function)

--- init.lua
+++ init.lua
@@ -11,7 +11,16 @@ minetest.register_chatcommand("symdon", {
      end
 })
 
+-- Login Bonus
 minetest.register_on_joinplayer(function (player)
-  local name = player:get_player_name()
-  minetest.chat_send_player(name, "メッセージをやって!!")
+      local name = player:get_player_name()
+      local inv = player:get_inventory()
+      local login_bonus = ItemStack("default:apple 1")
+      local message = string.format("Hi %s, You got a login bonus. (bonus: apple+1)", name)
+      -- 注意
+      -- ログインボーナスと言いつつ、ログインしたら必ず林檎を1つ貰える。
+      -- inventoryがいっぱいの場合、ログインボーナスは付与されない。
+      -- ログインボーナスが貰えても、貰えなくてもメッセージは同じように表示される。
+      inv:add_item("main", login_bonus)
+      minetest.chat_send_player(name, message)
 end)
