24683cfc6d2fb64bbf92184ea070c517094c4974
TakesxiSximada <sximada@gmail.com>
2023-04-02 09:38:39 +0900

    fix: be able to specify special bonus days to minetest.conf

--- init.lua
+++ init.lua
@@ -15,8 +15,6 @@ minetest.register_chatcommand("symdon", {
 minetest.register_on_joinplayer(function (player)
       local name = player:get_player_name()
       local inv = player:get_inventory()
-      local login_bonus = ItemStack("default:apple 1")
-      local message = string.format("Hi %s, You got a login bonus. (bonus: apple+1)", name)
       local player_meta = player:get_meta()
       local last_timestamp = player_meta:get_int("login_bonus:last_timestamp")
       local current_timestamp = os.time(os.date("!*t"))
@@ -27,14 +25,28 @@ minetest.register_on_joinplayer(function (player)
 
       -- 当日付与済みであるため付与しない
       if last_date == current_date then
-	 minetest.chat_send_player(name, "Welcome home")
-	 do return end
+         minetest.chat_send_player(name, "Welcome home")
+         do return end
       end
 
       -- 注意
       -- inventoryがいっぱいの場合、ログインボーナスは付与されない。
       -- その場合であっても、メッセージは同じように表示される。
-      inv:add_item("main", login_bonus)
+      local today = os.date("%m%d", current_timestamp + 32400)
+      local message = string.format("Hi %s, You got a login bonus. (bonus: apple+1)", name)
+
+      -- 設定からスペシャルログインボーナスデーを取得し、スペシャルログインボーナスデーかどうかを判定する。
+      -- スペシャルログインボーナスデーはmmdd形式で、複数指定する場合はカンマで区切る。
+      local special_days = minetest.settings:get("symdon_special_days") or ""
+      if string.match(special_days, today) then
+         message = string.format("Hi %s, You got a login special bonus. (special bonus: diamond+1)", name)
+         local login_bonus_special = ItemStack("default:diamond 1")
+         inv:add_item("main", login_bonus_special)
+      else
+         local login_bonus = ItemStack("default:apple 1")
+         inv:add_item("main", login_bonus)
+      end
+
       player_meta:set_int("login_bonus:last_timestamp", current_timestamp)
       minetest.chat_send_player(name, message)
 end)
